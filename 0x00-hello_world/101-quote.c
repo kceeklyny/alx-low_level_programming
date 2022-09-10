1bbe620dc74cec64aed7e8ed00513f538299ac02
kceeklyny <109355818+kceeklyny@users.noreply.github.com>
2022-09-10 11:57:32 +0100

    Advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - "
-			"Dora Korpar, 2015-10-19\n", 59);
+	char ch[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(STDOUT_FILENO, ch, 59);
 	return (1);
 
 }
