3cb2579073b2a0b5e824ae25ebbee09db1f768eb
kceeklyny <chukukelvin11@gmail.com>
2022-09-13 14:27:01 -0700

    Task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,10 +4,10 @@
  * @c: char type letter
  * Return: 1 if lowercase, 0 if not lowercase
  */
-int _islower(int c)
+int _islower (int c)
 {
 	if (c > 96 && c < 123)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
