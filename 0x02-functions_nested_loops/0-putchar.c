675261d5e2a729f7906243b73e2e40894007aa67
kceeklyny <chukukelvin11@gmail.com>
2022-09-13 01:00:13 -0700

    1st Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+	return (0);
+}
