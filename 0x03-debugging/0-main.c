c018bc0187d966c8a05ebded44eb42ee4bd97b99
kceeklyny <chukukelvin11@gmail.com>
2022-09-14 06:19:53 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-* positive_or_negative - is a function that checks for positive numbers
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
